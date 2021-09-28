47c37600656acbb7e4589d2b50abe0f3dadbf894
rhit-labayojt <labayojt@rose-hulman.edu>
2021-09-28 12:42:46 -0400

    First turn in

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+
+//Scroll to Top Button
+scrollbutton = document.getElementById("scrollBtn");
+
+
+// When scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollbutton.style.display = "block";
+  } else {
+    scrollbutton.style.display = "none";
+  }
+}
+
+// When click on the button, smooth scroll to the top
+function topFunction() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+}
